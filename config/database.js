ce234fcec73cbb0cb6f2f06038915e87b3ed8659
TNORTHERN <traynorthern@yahoo.com>
2020-06-17 08:35:39 -0500

    removing unneccessary env vars

--- config/database.js
+++ config/database.js
@@ -6,15 +6,9 @@ if (process.env.NODE_ENV === "production") {
         connector: "mongoose",
         settings: {
           client: "mongo",
-          host: env("DATABASE_HOST", "localhost"),
-          port: env.int("DATABASE_PORT", 27017),
-          database: env("DATABASE_NAME", "strapi"),
-          username: env("DATABASE_USERNAME", "strapi"),
-          password: env("DATABASE_PASSWORD", "strapi"),
           uri: env("DATABASE_URI"),
         },
         options: {
-          authenticationDatabase: env("AUTHENTICATION_DATABASE"),
           ssl: true,
         },
       },
