9b6093d604fd851c03261cdcb742033fbe6cab0c
Brandon Leafman <17711952+Daxo@users.noreply.github.com>
2020-06-22 15:58:08 -0500

    WIP, successfully get all content types and their schemas

--- plugins/make-fake/controllers/make-fake.js
+++ plugins/make-fake/controllers/make-fake.js
@@ -15,11 +15,28 @@ module.exports = {
    */
 
   index: async (ctx) => {
-    // Add your own logic here.
+    // returns all content types and their schema
 
+    const contentTypeService = strapi.plugins['content-type-builder'].services.contenttypes;
+
+    const contentTypes = Object.keys(strapi.contentTypes)
+    .filter(uid => {
+      if (uid.startsWith('strapi::')) return false;
+      if (uid === 'plugins::upload.file') return false; // TODO: add a flag in the content type instead
+
+      return true;
+    })
+    .map(uid => contentTypeService.formatContentType(strapi.contentTypes[uid]));
+
+    ctx.send({
+      message: contentTypes
+    });
+  },
+  healthCheck: async (ctx) => {
     // Send 200 `ok`
     ctx.send({
       message: 'ok'
     });
-  }
+  },
+
 };
